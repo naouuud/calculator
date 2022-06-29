175149d9895ff7ea412be1b9f3e259def9787b02
Nour Aoude <naoude@gmail.com>
2022-06-29 14:54:47 +0300

    Functional calculator

--- script.js
+++ script.js
@@ -5,32 +5,83 @@ const operations = {
     divide: (a, b) => a / b,
 }
 
+const operands = {
+    a: '0',
+    b: ''
+}
+
 function operate(a, b, operator) {
     return operations[operator](a, b);
 }
 
 const display = document.querySelector(".display");
-const buttons = document.querySelectorAll(".button");
+const numbers = document.querySelectorAll(".number");
+const commands = document.querySelectorAll(".command");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
 
-buttons.forEach(button => button.addEventListener('click', e => calculate(e)));
+numbers.forEach(number => number.addEventListener('click', e => fillOperands(e)));
+commands.forEach(command => command.addEventListener('click', e => selectOperator(e))); 
+equals.addEventListener('click', e => calculate(e));
+clear.addEventListener('click', clearCalc);
 
-let a;
-let b;
+let initialized = false;
+let opInitialized = false;
+let fillB = false;
+let calcFinished = false;
 let operator;
 
+function fillOperands(e) {
+    if (calcFinished) clearCalc();
+    if (!fillB) {
+    if (!initialized) {
+        operands.a = '';
+        operands.a += e.target.id;
+        display.textContent = operands.a;
+        initialized = true;
+    } else {
+        operands.a += e.target.id;
+        display.textContent = operands.a;
+    }
+    } else {
+        operands.b += e.target.id;
+        display.textContent = operands.b;
+    }
+}
+
+function selectOperator(e) {
+    if (!opInitialized) {
+    operator = e.target.id;
+    fillB = true;
+    opInitialized = true;
+    } else {
+        calculate(e);
+        operator = e.target.id;
+        calcFinished = false;
+    }
+}
+
 function calculate(e) {
-    if (e.target.classList[1] == "number") {
-        display.textContent += e.target.id;
-    }
-    else if (e.target.classList[1] == "command") {
-        if (a === undefined) {
-            a = parseInt(display.textContent);
-            operator = e.target.id;
-            display.textContent = '';
-        }
-        else {
-            b = parseInt(display.textContent);
-            display.textContent = operate(a, b, operator); 
-        }
-    }
+    if (operator) {
+    a = parseFloat(operands.a);
+    b = parseFloat(operands.b);
+    if (!b) return;
+    let result = (operate(a, b, operator));
+    display.textContent = result;
+    operands.a = result;
+    operands.b = '';
+    operator = '';
+    calcFinished = true;
+    } else return;
+}
+
+function clearCalc() {
+    operands.a = '0';
+    operands.b = '';
+    operator = '';
+    initialized = false;
+    opInitialized = false;
+    fillB = false
+    calcFinished = false;
+    display.textContent = '';
 }
